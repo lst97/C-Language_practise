e657e1df6341db5b12ff2b1ad893ec8e50a2ae90
lst97 <laisiotou1997@gmail.com>
2018-12-14 00:15:42 +0930

    Update 051107_programmingPractise.c

--- 0511/051107_programmingPractise.c
+++ 0511/051107_programmingPractise.c
@@ -12,36 +12,39 @@
 ;*
 ;* Copyright   		 : GNU GENERAL PUBLIC LICENSE Version 3
 ;*
-;* Purpose           : DEBUG added. Ansi-style
+;* Purpose           : Convert day to year or weeks + day. Ansi-style
 ;*
 ;* Revision History  :
 ;*
 ;* Date        Author      Ref    Revision (Date in DDMMYYYY format)
 ;* 13122018    lst97       1      First release
 ;* 13122018    lst97       2      Fix pCOMMAND() function bug
+;* 14122018    lst97       3      1.Fully fix pCOMMAND() bug. 2.Performance improved by changing to switch instead of if-else.
 ;* Known Issue       :
 ;*
-;* 1.COMMAND mode problem scanf() 2.No error handling when user input non-integer type.
+;* 1.No error handling when user input non-integer type.
 ;|**********************************************************************;
 */
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
+#define TRUE 1
+#define FALSE 0
 #define SPACEBAR 0x20
 #define BINARY 2
+
+
 #define BIT 8
 #define DAY 7
 #define WEEK 4
 #define MOUNTH 12
-#define TRUE 1
-#define FALSE 0
 
 int pCOMMAND(void);
 int pDEBUG(void);
-int pEND(void);
+_Bool pEND(void);
 
-_Bool debugSwitch = 1;
+_Bool debugSwitch = TRUE;
 int main(void) {
 	const unsigned short int numLimited;
 	unsigned int userInput;
@@ -52,47 +55,52 @@ int main(void) {
 	userInput_temp = userInput;
 
 	printf("%u Day = %u Year / %u Weeks + %u Days", userInput, userInput_temp / (DAY * WEEK * MOUNTH), userInput_temp / DAY, userInput_temp % DAY);
-	if(debugSwitch == 1){printf("\t*DEBUG[userInput]= '%X'\t[userInput_temp]= '%X'\t", userInput, userInput_temp);}
+	if(debugSwitch == 1)printf("\t*DEBUG[userInput]= '%X'\t[userInput_temp]= '%X'\t", userInput, userInput_temp);
+	while (getchar() != '\n');
 
 	printf("\n\nEnter q to exit\n");
 	pCOMMAND();
+	printf("Program terminated normally\n");
 	return 0;
 }
 
 int pCOMMAND(void){
-	const unsigned short int arrayNum = 2;
-		char userCommand[arrayNum];
+	const unsigned short int arrayNum = 1;
+	char userCommand[arrayNum];
+	_Bool loopSwitch = TRUE;
 
-		while(TRUE){
-			scanf("%s", userCommand);
-			if(strlen(userCommand) == 1){
-				if(userCommand[0] == 'd'){
-					pDEBUG();
-					userCommand[0] = SPACEBAR;
-					pCOMMAND();
-				}else if (userCommand[0] == 'q'){
-					pEND();
-				}
-			}
+	do{
+		scanf("%s", userCommand);
+		if(strlen(userCommand) == 1){
+			switch (userCommand[0]){
+			case 'd':
+				pDEBUG();
+				userCommand[0] = SPACEBAR;
+				continue;
+			case 'q':
+				pEND();
+				loopSwitch = (int)pEND();
+				break;
+			default:
 			printf("%s: Command not found!\n", userCommand);
-			userCommand[0] = SPACEBAR;
+			}
+		}else {
+			printf("%s: Command not found!\n", userCommand);
+			while (getchar() != 0x000A);
 		}
-		return 0;
+	}while(loopSwitch);
+	return 0;
 }
 
 int pDEBUG(void){
-	if(debugSwitch == TRUE){
-		debugSwitch = FALSE;
-	}else{
-		debugSwitch = TRUE;
-	}
+	if(debugSwitch == TRUE)debugSwitch = FALSE;
+	else debugSwitch = TRUE;
 	printf("Debug Mode = %u\n", debugSwitch);
 	return 0;
 }
 
-int pEND(void){
-	printf("Program terminated normally\n");
-	exit(0);
+_Bool pEND(void){
+	return 0;
 }
 /*RESULT
 Please enter a day [0 ~ 65535]

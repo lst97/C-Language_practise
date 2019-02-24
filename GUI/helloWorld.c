774179d4be688330bddea13a736d2965bdf1ae43
lst97 (Nelson) <laisiotou1997@gmail.com>
2019-02-23 20:54:52 -0800

    messageBox using C Language

---
+++ GUI/helloWorld.c
@@ -0,0 +1,35 @@
+/*
+;|**********************************************************************;
+;* Project           : Basic_C_GUI
+;*
+;* Program name      : helloWorld.c
+;*
+;* Author            : SIO TOU LAI (laisiotou1997@gmail.com)
+;*
+;* Date created      : 24/02/2019
+;*
+;* Version           : Internal
+;*
+;* Copyright         : GNU GENERAL PUBLIC LICENSE Version 3
+;*
+;* Purpose           : messageBox using C Language
+;*
+;* Revision History  :
+;*
+;* Date        Author      Ref    Revision (Date in DDMMYYYY format)
+;* 24022019    lst97       1      First release
+;*
+;* Known Issue       :
+;*
+;|**********************************************************************;
+*/
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
+	static char szContent[] = TEXT("Hello World");
+	static char szCaption[] = TEXT("Message");
+
+	MessageBox(NULL, szContent, szCaption, MB_OK);
+	return 0;
+}

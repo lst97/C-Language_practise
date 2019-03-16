73567653ff1e6094530651e6416076b7c0950884
SIO TOU LAI (Nelson) <laisiotou1997@gmail.com>
2019-03-16 14:59:24 +1100

    Drawing using mouseEvent (client area)
    /* Reference */
    https://www.bilibili.com/video/av10340284/?p=37 | https://fishc.com

---
+++ GUI/Keybord&Mouse/basicMouseEvent.c
@@ -0,0 +1,147 @@
+/*
+;|**********************************************************************;
+;* Project           : Basic_C_GUI
+;*
+;* Program name      : basicMouseEvent.c
+;*
+;* Author            : SIO TOU LAI (laisiotou1997@gmail.com)
+;*
+;* Date created      : 16/03/2019
+;*
+;* Version           : Internal
+;*
+;* Copyright         : GNU GENERAL PUBLIC LICENSE Version 3
+;*
+;* Purpose           : Drawing using mouseEvent (client area)
+;*
+;* Revision History  :
+;*
+;* Date        Author      Ref    Revision (Date in DDMMYYYY format)
+;* 16032019    lst97       1      First release
+;* 
+;* Known Issue       :
+;*
+;|**********************************************************************;
+*/
+/* Reference */
+// https://www.bilibili.com/video/av10340284/?p=37 | https://fishc.com
+
+// Header
+#include <windows.h>
+#include <windowsx.h>
+
+// Define
+#define MAXPOINTS 2048
+
+// Declar
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// DATA
+static char szAppName[] = "Basic Keyboard&Mouse - Mouse Event";
+static char szErrorMessage[] = "This program only run on Windows NT!";
+static unsigned int nClientWidth = 640;
+static unsigned int nClientHeight = 480;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow) {
+
+	//WNDCLASS -> RegisterClass -> CreateWindow -> ShowWindow -> UpdateWindow -> MsgLoop
+
+	WNDCLASS wndClass;
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;						//Redraws the entire window if a movement or size adjustment changes.
+	wndClass.lpfnWndProc = WndProc;									//A pointer to the window procedure.
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;									//A handle to the instance that contains the window procedure for the class.
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//A handle to the class background brush.
+	wndClass.lpszMenuName = NULL;									//The resource name of the class menu, as the name appears in the resource file.
+	wndClass.lpszClassName = szAppName;								//The maximum length for lpszClassName is 256.
+
+	if (!RegisterClass(&wndClass)) {								//Value is zero if Register FAIL (!0) = TRUE;
+		MessageBox(NULL, szErrorMessage, szAppName, MB_ICONERROR);
+		return 0;
+	}
+
+	HWND hWnd;
+	hWnd = CreateWindow(szAppName, szAppName, WS_TILED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT, nClientWidth, nClientHeight, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, SW_SHOWNORMAL);		// nCmdShow
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	// DATA
+	static POINT pt[MAXPOINTS];
+	static unsigned int pointCounter;
+	HDC hDc;
+	PAINTSTRUCT ps;
+
+	// CODE
+	switch(message){
+	case WM_LBUTTONDOWN:
+		pointCounter = NULL;
+		InvalidateRect(hWnd, NULL, TRUE);
+		return 0;
+
+	case WM_MOUSEMOVE:
+		// Saving posistion to pt array
+		if (wParam == MK_LBUTTON && pointCounter < MAXPOINTS) {	// you can use == instead of & so you can control when to draw by using rightbutton
+			pt[pointCounter].x = GET_X_LPARAM(lParam);
+			pt[pointCounter].y = GET_Y_LPARAM(lParam);
+			pointCounter++;
+
+			hDc = GetDC(hWnd);
+
+			SetPixel(hDc, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), 0);
+
+			ReleaseDC(hWnd, hDc);
+		}
+		return 0;
+
+	case WM_LBUTTONUP:
+		// Active WM_PAINT
+		InvalidateRect(hWnd, NULL, FALSE);
+		return 0;
+
+	case WM_PAINT:
+		hDc = BeginPaint(hWnd, &ps);
+
+		// Loading cursor
+		SetCursor(LoadCursor(NULL, IDC_WAIT));
+		ShowCursor(TRUE);
+
+		// Connect each points algo
+		for (unsigned int rowPosition = 0; rowPosition < pointCounter; rowPosition++) {
+			for (unsigned int colPosition = rowPosition + 1; colPosition < pointCounter; colPosition++) {
+				MoveToEx(hDc, pt[rowPosition].x, pt[rowPosition].y, NULL);
+				LineTo(hDc, pt[colPosition].x, pt[colPosition].y);
+			}
+		}
+
+		ShowCursor(FALSE);
+		SetCursor(LoadCursor(NULL, IDC_ARROW));
+
+		EndPaint(hWnd, &ps);
+		return 0;
+
+	case WM_CLOSE:
+		switch (MessageBox(hWnd, TEXT("Do you really want to quit?"), TEXT("Message"), MB_YESNO | MB_ICONQUESTION)) {
+		case IDYES:
+			PostQuitMessage(0);
+			return 0;
+		default:
+			break;
+		}
+		return 0;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}

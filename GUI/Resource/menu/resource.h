8f9373443d5e4065c8f908d1d236b6f8132895d1
SIO TOU LAI (Nelson) <laisiotou1997@gmail.com>
2019-04-14 17:20:17 +1000

    Add files via upload

---
+++ GUI/Resource/menu/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDI_ICON                        101
+#define IDR_MENU                        102
+#define IDS_STRING_ABOUT                103
+#define IDS_STRING_TITLE                104
+#define IDS_STRING_OSERROR              105
+#define ID_HELP_ABOUT                   40001
+#define ID_SETTING_CHANGEBGCOLOR        40002
+#define ID_CHANGEBGCOLOR_RED            40003
+#define ID_CHANGEBGCOLOR_GREEN          40004
+#define ID_CHANGEBGCOLOR_BLUE           40005
+#define ID_CHANGEBGCOLOR_RANDOM         40006
+#define ID_PROGAM_EXIT                  40007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

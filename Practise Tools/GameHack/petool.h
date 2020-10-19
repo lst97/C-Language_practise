699505313d3ab8ceb45df8696f67440a892ff55e
SIO TOU LAI [Nelson] <laisiotou1997@gmail.com>
2020-10-19 17:23:08 +1100

    Update petool.h

--- Practise Tools/GameHack/petool.h
+++ Practise Tools/GameHack/petool.h
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "String.h"
+// if the funtion return value is 0xFFFFFFF: init heap FAIL
 
 #ifndef _WINDOWS_
 #define BYTE unsigned char
@@ -13,7 +14,7 @@
 #define OPTHEADER_SIZE 0x60
 
 // PE Header Offset
-#define SECTIONSIZE_OFFSET 0x06
+#define SECTIONNUM_OFFSET 0x06
 #define OPTHSIZE_OFFSET 0x14
 #define OPTH_OFFSET 0x18
 #define IFANEW_OFFSET 0x3C
@@ -23,11 +24,11 @@
 #define RAWSIZE_OFFSET 0x10
 #define RAWPTR_OFFSET 0x14
 #define VIRTUALADDR_OFFSET 0x0C
+#define EXE_CHARACTERISTIC 0x60000020
 
 // Uninit
 #define UNINIT_STACK 0xCCCCCCCC
 #define UNINIT_HEAP 0xCDCDCDCD
-
 
 typedef unsigned char Buffer;
 typedef Buffer FBuffer;
@@ -35,7 +36,6 @@ typedef Buffer IBuffer;
 typedef Buffer HBuffer;
 
 // PE struct
-
 struct OPT_HEADER {
 	WORD Magic;
 	BYTE MajorLinkerVersion;
@@ -69,13 +69,30 @@ struct OPT_HEADER {
 	DWORD NumberOfRvaAndSizes;
 };
 
+// Section struct
+struct SECTION_HEADER {
+	BYTE Name[8];
+	union {
+		DWORD PhysicalAddress;
+		DWORD VirtualSize;
+	} Misc;
+	DWORD VirtualAddress;
+	DWORD SizeOfRawData;
+	DWORD PointerToRawData;
+	DWORD PointerToRelocations;
+	DWORD PointerToLinenumbers;
+	WORD  NumberOfRelocations;
+	WORD  NumberOfLinenumbers;
+	DWORD Characteristics;
+};
+
 // Program struct
 struct Header {
+	unsigned short SizeOfOptionalHeader;
+	unsigned short NumberOfSection;
 	HBuffer* pBuffer;
 	OPT_HEADER* pOptheader;
 	unsigned int e_lfanew;
-	unsigned short SizeOfOptionalHeader;
-	unsigned short NumberOfSection;
 	unsigned int SizeOfHeaders;
 	unsigned int SizeOfImage;
 
@@ -87,24 +104,29 @@ struct Header {
 
 FBuffer* fcreate();
 int fwrite(unsigned int offset, unsigned int size);
-unsigned int FoaToRva(unsigned int foa_addr, unsigned int rva_addr);
+unsigned int FoaToRva(unsigned int foa_addr);
+int newsection(const char* section_name, char* bcode, unsigned int bcode_size, unsigned int characteristics);
+int inject(const char* section_name, char* bcode);
 struct FileObj {
 	Header* pHeader;
 	FBuffer* pBuffer;
 	FBuffer* (*create)();
-	unsigned int (*foa_rva)(unsigned int, unsigned int);
-	int (*write)(unsigned int, unsigned int);
 	IBuffer* (*expand)();
+	int (*newsection)(const char*, char*, unsigned int, unsigned int);
+	int (*inject)(const char*, char*);
+	int (*write)(unsigned int, unsigned int);
+	unsigned int (*foa_rva)(unsigned int);
 };
 
 int iwrite(unsigned int offset, unsigned int size);
 unsigned int icompress(unsigned int* ptr);
+unsigned int RvaToFoa(unsigned int rva_addr);
 struct ImageObj {
 	Header* pHeader;
 	IBuffer* pBuffer;
-	unsigned int (*rva_foa)(unsigned int, unsigned int);
 	IBuffer* (*create)();
 	int (*write)(unsigned int, unsigned int);
+	unsigned int (*rva_foa)(unsigned int);
 	unsigned int (*compress)(unsigned int*);
 };
 
@@ -114,24 +136,25 @@ bool isx86();
 bool isx64();
 struct Validator {
 	bool (*ispe)();
-	int (*filecmp)(FBuffer*);
 	bool (*isx86);
 	bool (*isx64);
+	int (*filecmp)(FBuffer*);
 };
 
+// flags: overwrite(0); return: -1 (file exit)
 int fexport(FBuffer* pBuffer, const char* filename, unsigned int size, unsigned int flags);
 struct PETool {
 	const char* pFilename;
+	int (*fexport)(FBuffer*, const char*, unsigned int, unsigned int);
 	Header* pHeader;
+	Validator validator;
 	FileObj file;
 	ImageObj image;
-	Validator validator;
-	int (*fexport)(FBuffer*, const char*, unsigned int, unsigned int);
 
 };
 
 PETool* PETool_new(const char* filename);
+Header* Header_new();
 Validator Validator_new();
 FileObj File_new();
 ImageObj Image_new();
-Header* Header_new();
